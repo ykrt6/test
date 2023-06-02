911ba07c9c750832076c0aeaa85de56e07d6b528
ykrt6 <8.ya9baka@gmail.com>
2023-06-02 15:38:47 +0900

    modefied pyfile

--- save.py
+++ save.py
@@ -1,18 +1,80 @@
+# streamlit系
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
-
-import cv2
-import datetime
-import time
-from PIL import Image
-import io
-import pandas as pd
 import av
 
-# import speech_recognition as sr
-
+# dropbox系
 import dropbox
+import io
+import time
+import datetime
+import pandas as pd
 
+# 画像処理系
+import numpy as np
+import cv2
+from PIL import Image
+
+
+## キャプチャーをするクラス
+class VideoProcessor:
+
+    def __init__(self) -> None:
+        self.save_state = "保存しない"
+
+    ## 画像アップロード
+    def uploadDropbox(self, img, filename, client) :
+        dropbox_path="/test/" + filename
+
+        client.files_upload(img, dropbox_path, mode=dropbox.files.WriteMode('overwrite'))
+        st.success(filename + "をアップロードしました")
+
+    ## 保存
+    def save(self, img) :
+        now = datetime.datetime.now()
+        now_second = now.second
+        if now_second % 10 == 0 :
+            filename = now.strftime('%Y%m%d_%H%M%S') + '.jpg'
+            img_bytes = io.BytesIO()    # メモリ上でバイナリデータを扱う
+            img.save(img_bytes, "JPEG")     # メモリ上に保存
+            img_bytes = img_bytes.getvalue()  # バッファすべてを bytes として出力
+            self.uploadDropbox(img_bytes, filename, client)    # dropboxにアップロード
+
+    ## 画像処理
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        if self.save_state == "保存する" :
+            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            self.save(img)
+            img = np.array(img)
+            img = av.VideoFrame.from_ndarray(img)
+        else :
+            img = av.VideoFrame.from_ndarray(img, format="bgr24")
+
+        return img
+
+
+## Dropbox アカウント情報入力
+def fileProcess() -> str :
+    uploaded_file = st.file_uploader("ファイルアップロード", type='csv')
+    if uploaded_file is not None:
+        # アップロードファイルをメインにデータ表示
+        df = pd.read_csv(uploaded_file, encoding="shift-jis", header=None)
+        if st.checkbox("データファイルの内容を表示") :    
+            st.table(df)
+    else :
+        st.warning("データファイルをアップロードしてください")
+        st.stop()
+
+    app_key = df.loc[0,1]
+    app_secret = df.loc[1,1]
+    refresh_token = df.loc[2,1]
+
+    return app_key, app_secret, refresh_token
+
+
+## Dropbox にリンク
 def linkDropbox(app_key, app_secret, refresh_token) :
     rdbx = dropbox.Dropbox(oauth2_refresh_token=refresh_token, app_key=app_key, app_secret=app_secret)
 
@@ -24,100 +86,26 @@ def linkDropbox(app_key, app_secret, refresh_token) :
     st.success("ドロップボックスアカウントがリンクされました")
     return client
 
-def uploadDropbox(img, filename, client) :
-    dropbox_path="/test/" + filename
 
-    client.files_upload(img, dropbox_path)
-    st.success(filename + "をアップロードしました")
-
-# def record() :
-#     listener = sr.Recognizer()
-#     try:
-#         with sr.Microphone() as source:
-#             st.write("Listening...")
-#             voice = listener.listen(source)
-#             voice_text = listener.recognize_google(voice, language="ja-JP")
-#             st.write(voice_text)
-#         return voice_text
-#     except:
-#         st.warning('sorry I could not listen')
-
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-def capter(cap, image_loc) -> Image :
-    img = webrtc_streamer(key="example", video_frame_callback=callback)
-    # ret, img = cap.read()
-    # time.sleep(0.01)
-    # img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    # image_loc.image(img)
-
-    return img
-
-def fileProcess() -> str :
-    uploaded_file = st.file_uploader("ファイルアップロード", type='csv')
-    if uploaded_file is not None:
-        # アップロードファイルをメインにデータ表示
-        df = pd.read_csv(uploaded_file, encoding="shift-jis", header=None)
-        if st.checkbox("データファイルの内容を表示") :    
-            st.table(df)
-    else :
-        st.warning("データファイルをアップロードしてください")
-        st.stop()
-        
-    app_key = df.loc[0,1]
-    app_secret = df.loc[1,1]
-    refresh_token = df.loc[2,1]
-
-    return app_key, app_secret, refresh_token
-
-
+## メイン
 def main(client) :
     # キャプチャーの準備
-    cap = cv2.VideoCapture(0)
-    image_loc = st.empty()
+    # cap = cv2.VideoCapture(0)
 
-    # ラジオボタン
-    option = st.selectbox("選択してください", ["none", "capter", "save"])
-    st.write(option + "を選択中")
+    # while cap.isOpened :
+    webrtc = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+    if webrtc.video_processor :
+        webrtc.video_processor.save_state = st.radio(label='選択してください', options=('保存しない', '保存する'), index=0, horizontal=True)
     
-    if option == "capter" :
-        # while cap.isOpened :
-        img = capter(cap, image_loc)    # キャプチャー
-    elif option == "save" :
-        filename = datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.jpg'
-        img = capter(cap, image_loc)
-        img_bytes = io.BytesIO()    # メモリ上でバイナリデータを扱う
-        img.save(img_bytes, "JPEG")     # メモリ上に保存
-        img_bytes = img_bytes.getvalue()  # バッファすべてを bytes として出力
-        uploadDropbox(img_bytes, filename, client)    # dropboxにアップロード
-    
-    cap.release()
-    # cv2.destroyAllWindows()
+    # cap.release()
 
 if __name__ == "__main__" :
-    # update_access_token = False
-    # client = ""
-
     # App key、App secret、更新用トークンを使ってアクセルトークンの自動更新
     app_key, app_secret, refresh_token = fileProcess()
    
     # dropbox とリンク付け
     client = linkDropbox(app_key, app_secret, refresh_token)
 
-    # streamlit の仕様のため
+    # streamlit の仕様のため (気にしないで)
     main(client)
-        
-
-    
-    # voice_text = record()
-
-    # if voice_text == "あ" :
-    #     capter()
-    # else :
-    #     st.warning(voice_text)
     
